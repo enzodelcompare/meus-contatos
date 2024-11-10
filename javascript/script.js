b5fcc5e4b8b339344d0c6b02a4b165ea306d8385
Enzo Delcompare <78515983+enzodelcompare@users.noreply.github.com>
2024-11-10 19:43:16 -0300

    Limpar formulário após envio

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,12 @@
-function clearForm() {
+//function clearForm() {
     // Limpa os campos do formulário
-    document.getElementById('contact-form').reset();
+//    document.getElementById('contact-form').reset();
+//}
+
+function clearForm() {
+    document.getElementById("name").value = '';
+    document.getElementById("email").value = '';
+    document.getElementById("interest").value = '';
+    document.getElementById("message").value = '';
+    alert("Formulário enviado com sucesso!");
 }
